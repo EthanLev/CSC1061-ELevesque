ff3364c1e1cfa8ce1fe17c6d225435eb4dc38cd4
Ethan <120345077+EthanLev@users.noreply.github.com>
2025-01-31 11:53:02 -0700

    Credit Card Validation Lab

--- CreditCardValidationProc/src/CreditCardValidation.java
+++ CreditCardValidationProc/src/CreditCardValidation.java
@@ -1,45 +1,41 @@
 import java.util.Scanner;
 
 public class CreditCardValidation {
-
+	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Enter credit card number");
 		String cardNum = input.nextLine();
 		
-		// Check and print if card if valid or not
-		if (isValid(cardNum)) {
-			System.out.println("This number is valid");
-		} else {
-			System.out.println("This number is invalid");
-		}
+		// print if card number is valid or not
+		
 	}
 	
-	// Returns true if card number is valid
-	public static boolean isValid(String num) {
-		// must have 13 - 16 digits
-		if (num.length() < 13 || num.length() > 16) {
+	// Return true if card number is valid
+	public static boolean isValid(String cardNum) {
+		// must have 13-16 digits
+		if (cardNum.length() < 13 || cardNum.length() > 16) {
 			return false;
 		}
 		
 		// must start with 4, 5, 6, or 37
-		char firstNum = num.charAt(0);
-		if (!(firstNum == '4' || firstNum == '5' || firstNum == '6' || num.startsWith("37"))) {
+		char firstDigit = cardNum.charAt(0);
+		if (!(firstDigit == '4' || firstDigit == '5' || firstDigit == '6' || cardNum.startsWith("37"))) {
 			return false;
 		}
 		
-		// if result is divisible by 10, is valid
+		// if divisible by 10, is valid
 		return true;
 	}
 	
 	// double every second digit from right to left
-	// if doubling makes it a 2 digit number, add them to get a single digit
+	// if doubling makes a 2 digit number, add them to get a single digit
 	
 	// add all single digit numbers
 	
 	// add all digits in odd places from right to left
 	
 	// add results
-
+	
 }
