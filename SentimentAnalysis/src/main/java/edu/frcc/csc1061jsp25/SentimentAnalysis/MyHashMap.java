842079b724e2ace1557539036435c2a813261dca
EthanLev <ethancolemanlevesque@gmail.com>
2025-05-02 12:49:56 -0600

    Sentiment Analysis

---
+++ SentimentAnalysis/src/main/java/edu/frcc/csc1061jsp25/SentimentAnalysis/MyHashMap.java
@@ -0,0 +1,177 @@
+package edu.frcc.csc1061jsp25.SentimentAnalysis;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class MyHashMap <K, V> implements Map <K,V> {
+	private static int INITIAL_NUM_BUCKETS = 4;
+	private static double LOAD_FACTOR_THRESHOLD = 0.5;
+	
+	private List<Entry<K,V>>[] buckets;
+	private int size = 0;
+	
+	private class Entry <K, V> implements Map.Entry<K, V> {
+		K key;
+		V value;
+		
+		public Entry(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		@Override
+		public K getKey() {
+			return key;
+		}
+
+		@Override
+		public V getValue() {
+			return value;
+		}
+
+		@Override
+		public V setValue(V value) {
+			V oldVal = this.value;
+			oldVal = value;
+			return value;
+		}
+	}
+	
+	public MyHashMap() {
+		buckets = new LinkedList[INITIAL_NUM_BUCKETS];
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		return false;
+	}
+
+	@Override
+	public V get(Object key) {
+		return null;
+	}
+
+	@Override
+	public V put(K key, V value) {
+		int bucketIndex = Math.abs(key.hashCode()) % buckets.length;
+		
+		// Check load factor
+		// Load factor = num entries in map / num of buckets
+		int loadFactor = size()/buckets.length;
+		if (loadFactor > LOAD_FACTOR_THRESHOLD) { 
+			rehash();
+			bucketIndex = Math.abs(key.hashCode()) % buckets.length;
+		}
+		
+		if (buckets[bucketIndex] == null) {
+			buckets[bucketIndex] = new LinkedList<>();
+		}
+		
+		for (Entry<K,V> entry: buckets[bucketIndex]) {
+			if (entry.key.equals(key)) {
+				V oldVal = entry.getValue();
+				entry.value = value;
+				return oldVal;
+			}
+		}
+		
+		// If key does not exist, insert key and value pair
+		buckets[bucketIndex].add(new Entry<K,V>(key, value));
+		size++;
+		return null;
+	}
+	
+	private void rehash() {
+		// New array twice the size
+		List<Entry<K,V>>[] newBuckets = new LinkedList[buckets.length * 2];
+		
+		for (int i = 0; i < newBuckets.length; i++) {
+			newBuckets[i] = new LinkedList<>();
+		}
+		
+		// Loop existing buckets
+		for (int i = 0; i < buckets.length; i++) {
+			if (buckets[i] != null) {
+				for (Entry<K,V> entry : buckets[i]) {
+					// Calculate new index
+					int newIndex = Math.abs(entry.key.hashCode()) % newBuckets.length;
+					
+					// Insert entry into new bucket
+					newBuckets[newIndex].add(entry);
+				}
+			}
+		}
+		
+		// Set buckets to new array
+		buckets = newBuckets;
+	}
+	
+	@Override
+	public V remove(Object key) {
+		int bucketIndex = Math.abs(key.hashCode()) % buckets.length;
+		
+		if (buckets[bucketIndex] != null) {
+			int listIndex = -1;
+			
+			for (Entry<K,V> entry: buckets[bucketIndex]) {
+				listIndex++;
+				
+				if (entry.key.equals(key)) {
+					break;
+				}
+			}
+			
+			V val = buckets[bucketIndex].get(listIndex).value;
+			
+			buckets[bucketIndex].remove(buckets[bucketIndex].get(listIndex));
+			size--;
+			return val;
+		}
+		
+		return null;
+	}
+
+	@Override
+	public void putAll(Map<? extends K, ? extends V> m) {
+		
+	}
+
+	@Override
+	public void clear() {
+		
+	}
+
+	@Override
+	public Set<K> keySet() {
+		return null;
+	}
+
+	@Override
+	public Collection<V> values() {
+		return null;
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<K, V>> entrySet() {
+		return null;
+	}
+}
+
---
+++ SentimentAnalysis/src/main/java/edu/frcc/csc1061jsp25/SentimentAnalysis/SentimentAnalysisTest.java
@@ -0,0 +1,59 @@
+package edu.frcc.csc1061jsp25.SentimentAnalysis;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class SentimentAnalysisTest {	
+	private static String FILE_NAME = "sentiments.txt";
+	
+	public static void main(String[] args) throws FileNotFoundException {
+	    // Create hash map
+	    MyHashMap<String, Integer> sentiments = new MyHashMap<>();
+	    
+	    // Load data from file to hash map
+	    loadFile(sentiments);
+	    
+	    // User input
+	    Scanner scnr = new Scanner(System.in);
+	    
+	    while (true) {
+	        System.out.println("Enter a phrase or type END to quit");
+	        String command = scnr.nextLine();
+	        
+	        if (command.equals("END")) {
+	            break;
+	        }
+	        
+	        // Remove punctuation, convert to lowercase, and split the words
+	        String[] words = command.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+	        
+	        for (String word : words) {
+				System.out.println("Processed: " + word);
+				
+				
+			}
+	    }
+	    
+	    scnr.close();
+	}
+
+	
+	private static void loadFile(MyHashMap<String, Integer> sentiments) throws FileNotFoundException {
+		Scanner scnr = new Scanner(new File(FILE_NAME));
+		
+		while (scnr.hasNextLine()) {
+			String line = scnr.nextLine();
+			String[] parts = line.split("\t"); // Split key and value from tab space
+			
+			if (parts.length == 2) { // Seperate into parts
+				String word = parts[0];
+				int sentiment = Integer.parseInt(parts[1]);
+				
+				sentiments.put(word, sentiment); // Add to map
+			}
+		}
+		
+		scnr.close();
+	}
+}
